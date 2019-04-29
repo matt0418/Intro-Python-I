c2067cae84a7a7aeaf883f4ddd7189b7b0ac570a
matt0418 <mfeldma7@gmail.com>
2019-04-29 17:10:33 -0400

    args.py completed

--- src/args.py
+++ src/args.py
@@ -17,9 +17,10 @@ print(f1(1, 2))
 # YOUR CODE HERE
 
 def f2(*args):
+    x = 0
     for arg in args:
-        x = arg
-        return x
+        x = x + arg
+    return x
 
 
 print(f2(1))                    # Should print 1
@@ -27,19 +28,22 @@ print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
-# a = [7, 6, 5, 4]
+a = [7, 6, 5, 4]
 
-# What thing do you have to add to make this work?
-# print(f2(a))    # Should print 22
+# What thing do you have to add to make this work? YOU NEED TO ADD AN *
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(a,b=1):
+    x = a + b
+    return x
 
-# print(f3(1, 2))  # Should print 3
-# print(f3(8))     # Should print 9
+print(f3(1, 2))  # Should print 3
+print(f3(8))     # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -51,22 +55,25 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for k,v in kwargs.items():
+        print(f"key: {k}, value: {v}")
 
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-# f4(a=12, b=30)
+f4(a=12, b=30)
 
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-# f4(city="Berkeley", population=121240, founded="March 23, 1868")
+f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-# d = {
-#     "monster": "goblin",
-#     "hp": 3
-# }
+d = {
+    "monster": "goblin",
+    "hp": 3
+}
 
 # What thing do you have to add to make this work?
-# f4(d)
+f4(**d)
